6a2b833c5da294c32655806d620c64433601d03a
Alexey Besedin <cdr800mb@mail.ru>
2023-08-18 03:14:42 +0400

    Plug in thymeleaf. Add main page functionality

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,13 +1,18 @@
 package hexlet.code;
 
+import hexlet.code.controllers.RootController;
 import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinThymeleaf;
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 public class App {
 
     public static void main(String[] args) {
         getApp().start(getPort());
     }
-
     private static int getPort() {
         String port = System.getenv().getOrDefault("PORT", "8080");
         return Integer.valueOf(port);
@@ -22,7 +27,21 @@ public class App {
     }
 
     public static void addRoutes(Javalin app) {
-        app.get("/", ctx -> ctx.result("Hello World"));
+        app.get("/", RootController.welcome);
+    }
+
+    private static TemplateEngine getTemplateEngine() {
+        TemplateEngine templateEngine = new TemplateEngine();
+
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("/templates/");
+        templateResolver.setCharacterEncoding("UTF-8");
+
+        templateEngine.addTemplateResolver(templateResolver);
+        templateEngine.addDialect(new LayoutDialect());
+        templateEngine.addDialect(new Java8TimeDialect());
+
+        return templateEngine;
     }
 
     public static Javalin getApp() {
@@ -30,6 +49,7 @@ public class App {
             if (!isProduction()) {
                 config.plugins.enableDevLogging();
             }
+            JavalinThymeleaf.init(getTemplateEngine());
         });
 
         addRoutes(app);
