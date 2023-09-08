cf5c7085e214033d4b4099b4d7945646cfafe7cc
Alexey Besedin <cdr800mb@mail.ru>
2023-09-09 02:22:03 +0400

    Test5

--- app/src/main/java/hexlet/code/domains/UrlCheck.java
+++ app/src/main/java/hexlet/code/domains/UrlCheck.java
@@ -3,7 +3,14 @@ package hexlet.code.domains;
 import io.ebean.Model;
 import io.ebean.annotation.WhenCreated;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.Column;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.JoinColumn;
+
 import java.time.Instant;
 
 @Entity
