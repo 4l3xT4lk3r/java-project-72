9b32c98e3e458de438af583fda1d76b1d336b57e
Alexey Besedin <cdr800mb@mail.ru>
2023-09-09 22:23:33 +0400

    Fix Unused import - javax.persistence.Lob.

--- app/src/main/java/hexlet/code/domains/UrlCheck.java
+++ app/src/main/java/hexlet/code/domains/UrlCheck.java
@@ -7,7 +7,6 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.persistence.Id;
 import javax.persistence.Column;
-import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.JoinColumn;
 
